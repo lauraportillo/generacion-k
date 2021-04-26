79f1eae6f5afe0fcc73461207c25023b85a37655
Laura Portillo <lauraportillo.ark@gmail.com>
2021-04-26 10:34:09 +0200

    add files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Generación K
+
+Hola! Soy Laura Portillo Rodríguez y esta es mi propuesta para resolver el reto para participar en la quinta edición de la Generación K.
---
+++ src/js/main.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const solutionContainer = document.querySelector('.js-solution');
+
+const message = 'Cómo será dar con Nekgikis V...';
+const numbers = [23, 24, 25, 18, 19, 5, 6, 7, 20, 15, 17, 8, 10, 11, 4, 3, 12, 2, 16, 14, 9, 21, 0, 1, 13, 22];
+
+const messageNoSpaces = message.replace(/ /g, '');
+console.log(messageNoSpaces);
+
+const messageLowecase = messageNoSpaces.toLowerCase();
+console.log(messageLowecase);
+
+const removeAccents = (str) => {
+  return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+};
+
+const messageClean = removeAccents(messageLowecase);
+console.log(messageClean);
+
+let data = [];
+console.log(data);
+function getData() {
+  for (let i = 0; i < messageClean.length; i++) {
+    const letter = messageClean[i];
+    const id = numbers[i];
+    data.push({ letter, id });
+  }
+}
+getData();
+
+const paintSolution = () => {
+  let html = '';
+  for (let i = 0; i < data.length; i++) {
+    const letter = data[i].letter;
+
+    html += `<span>`;
+    html += `${letter}`;
+    html += `</span>`;
+  }
+  solutionContainer.innerHTML = html;
+};
+
+data.sort(function (a, b) {
+  return a.id - b.id;
+});
+
+paintSolution();
